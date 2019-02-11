a71493ca80765ad5c600b8f5fa399d820f9eb7a5
Allen F Partlow <allenfp@gmail.com>
2019-02-11 05:49:57 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,6 +4,7 @@ def reformat_languages(languages)
 
   languages.each do |paradigm, data|
     data.each do |lang_name, data_|
+
       new_hash[lang_name] = {
         :type => data_[:type],
         :style => [paradigm]
